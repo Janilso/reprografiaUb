bbe6d6d73af98cb5ce2c133044afa8a2d575a6b0
Janilso <janilsorodrigues10@gmail.com>
2019-12-14 20:20:10 -0300

    Add animation in scrool slide to dow, bg color and credit counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:reprografiaub/screen/homeScreen.dart';
+import 'package:reprografiaub/screen/home/homeScreen.dart';
 
 void main() => runApp(MyApp());
 
--- lib/screen/home/homeScreen.dart
+++ lib/screen/home/homeScreen.dart
@@ -1,6 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:reprografiaub/utils/components/slidingUpDowPanel.dart';
-import '../utils/theme/style.dart';
+import '../../utils/theme/style.dart';
 
 class HomeScreen extends StatefulWidget {
   final title;
@@ -11,53 +13,133 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  bool changeColor = false;
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  Animation _arrowAnimation;
+  Animation _color1Animation;
+  Animation _color2Animation;
+  Animation _creditsAnimation;
+
+  AnimationController _arrowAnimationController;
+  AnimationController _colorAnimationController;
+  AnimationController _creditsAnimationController;
+
+  Size screenSize;
+
+  int credits = 100;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _arrowAnimationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    _colorAnimationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 10));
+    _creditsAnimationController =
+        AnimationController(duration: Duration(seconds: 1), vsync: this);
+
+    _arrowAnimation =
+        Tween(begin: 0.0, end: pi).animate(_arrowAnimationController);
+
+    _color1Animation = ColorTween(begin: Colors.transparent, end: primaryColor)
+        .animate(_colorAnimationController);
+    _color2Animation = ColorTween(begin: Colors.transparent, end: secondary)
+        .animate(_colorAnimationController);
+
+    _creditsAnimation = IntTween(begin: 0, end: credits).animate(
+        CurvedAnimation(
+            parent: _creditsAnimationController, curve: Curves.easeOut));
+
+    _creditsAnimationController.forward();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    screenSize = MediaQuery.of(context).size;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _creditsAnimationController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: Container(
           color: Colors.white,
-          child: /* Stack( */
-              // children: <Widget>[
-              _buildInfoLayer(),
-          // ],
-          // ),
+          child: _buildInfoLayer(),
+        ),
+        floatingActionButton: new FloatingActionButton(
+          backgroundColor: primaryColor,
+          child: Icon(Icons.cached, color: Colors.white),
+          onPressed: () => _creditsAnimationController.forward(from: 0),
         ),
       ),
     );
   }
 
   Widget _buildInfoLayer() {
-    final screenSize = MediaQuery.of(context).size;
-    // print("Height===> ${screenSize.height}");
     final maxHeight = screenSize.height > 750
         ? 0.60 * screenSize.height
         : 0.80 * screenSize.height;
     final minHeight = 70.0;
     final sizeQR = screenSize.width * 0.8;
 
-    final panel = Container(
-        decoration: changeColor
-            ? BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  colors: [primaryColor, secondary],
+    return SlidingUpDownPanel(
+      color: Colors.transparent,
+      boxShadow: null,
+      slideDirection: SlideDirection.DOWN,
+      maxHeight: maxHeight,
+      minHeight: minHeight,
+      parallaxEnabled: true,
+      parallaxOffset: .5,
+      panel: _panel(sizeQR, maxHeight),
+      body: _bodyScren(),
+      onPanelSlide: (double pos) {
+        if (pos > 0.01 && pos < 0.03) {
+          _colorAnimationController.animateTo(1);
+        }
+        _arrowAnimationController.animateTo(pos);
+      },
+      onPanelClosed: () {
+        _colorAnimationController.animateTo(0);
+      },
+    );
+  }
+
+  Widget _panel(double sizeQR, double maxHeight) {
+    return Stack(
+      children: <Widget>[
+        AnimatedBuilder(
+            animation: _colorAnimationController,
+            builder: (context, child) {
+              return Container(
+                height: maxHeight,
+                width: screenSize.width,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [_color1Animation.value, _color2Animation.value],
+                  ),
+                  boxShadow: [
+                    _colorAnimationController.value == 0
+                        ? BoxShadow(color: Colors.transparent)
+                        : BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 7.0,
+                            spreadRadius: 5.0,
+                            offset: Offset(0.0, 5.0),
+                          ),
+                  ],
                 ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black26,
-                    blurRadius: 7.0,
-                    spreadRadius: 5.0,
-                    offset: Offset(0.0, 5.0),
-                  )
-                ],
-              )
-            : BoxDecoration(),
-        padding: EdgeInsets.all(10.0),
-        child: Column(
+              );
+            }),
+        Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
             Container(
@@ -80,32 +162,19 @@ class _HomeScreenState extends State<HomeScreen> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Icon(
-                    changeColor
-                        ? Icons.keyboard_arrow_up
-                        : Icons.keyboard_arrow_down,
-                    color: Colors.white)
+                AnimatedBuilder(
+                  animation: _arrowAnimationController,
+                  builder: (context, child) => Transform.rotate(
+                    angle: _arrowAnimation.value,
+                    child: Icon(Icons.keyboard_arrow_down, color: Colors.white),
+                  ),
+                ),
               ],
-            )
+            ),
+            SizedBox(height: 10.0),
           ],
-        ));
-
-    return SlidingUpDownPanel(
-      color: Colors.transparent,
-      boxShadow: null,
-      slideDirection: SlideDirection.DOWN,
-      maxHeight: maxHeight,
-      minHeight: minHeight,
-      parallaxEnabled: true,
-      parallaxOffset: .5,
-      panel: panel,
-      body: _bodyScren(),
-      onPanelSlide: (double pos) {
-        if (pos > 0.01 && pos < 0.03) setState(() => changeColor = true);
-      },
-      onPanelClosed: () {
-        setState(() => changeColor = false);
-      },
+        ),
+      ],
     );
   }
 
@@ -207,15 +276,20 @@ class _HomeScreenState extends State<HomeScreen> {
                         children: <Widget>[
                           Text("Créditos".toUpperCase(),
                               style: textSubTitlePrimary),
-                          Text("888", style: textPointsPrimary),
+                          AnimatedBuilder(
+                              animation: _creditsAnimationController,
+                              builder: (BuildContext context, Widget child) {
+                                return Text(
+                                  _creditsAnimation.value.toString(),
+                                  style: textPointsPrimary,
+                                );
+                              }),
                         ],
                       ),
                     ),
                   ),
                 ),
                 SizedBox(height: 50),
-                Icon(Icons.cached, color: primaryColor, size: 35),
-                SizedBox(height: 40),
               ],
             ),
           ),
